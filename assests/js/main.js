8e055ddc0c4dff5c860270495798e25081ebba1a
himanshukhosla123 <himanshukhosla123@gmail.com>
2017-07-11 13:58:24 +0530

    Add files via upload

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,72 +1,51 @@
-function addListenters(){
-    var prev=null;
-}
-function showResult(){
-    var container=document.getElementById("questionsContainer");
-//    console.log(TestJson);
-    var questions=container.children;
-    console.log(questions);
-    var questionAttempted=0;
-    var testScore=0;
-    container.innerHTML='<h3 id="qstnAttempt">Question Attempted : <span id="QuestionAttempted">'+questionAttempted+'</span></h3><h2 class="center">Your Score Is : <span id="testScore">'+testScore+'/'+TestJson.questions.length+'<span></h2><button class="btn btn-info submit_btn">Show Answers</button>';
-    $("button").eq(1).hide();
-    $("body").append('<button onclick="loginHomePage();" class="btn btn-info submit_btn btn-lg">Logout</button>');
-}
+(function courseColor(){ 
+    var temp=$(".course_box");
+//    console.log(temp);
+    for(var i=0;i<temp.length;i++){
+//        var color_class=temp[i].children[0].children[0].classList[2];
+//        console.log(color_class);
+        $(temp[i]).hover(function(){
+            var color_class=this.children[0].children[0].classList[2];
+            console.log(color_class);
+            $(this).addClass("bg_"+color_class);
+            $(this).css("border-right","5px");
+            $(this).css("color","white");
+        }
+    ,   function(){
+            var color_class=this.children[0].children[0].classList[2];
+            $(this).removeClass("bg_"+color_class);
+            $(this).css("background-color","#f8f8f8");
+            $(this).css("border-right","3px");
+            $(this).css("color","black");
+        });
 
-function userLogin(){
-    var username=$("#username")[0].value;
-    var password=$("#password")[0].value;
-    $(".sign_in").hide();
-    $(".login_image ,.login_details").show();
-    if(username!=""&&password!=""){
-    var userExists=false;
-    var rawFile=new XMLHttpRequest();
-//    rawFile.overrideMimeType("application/json");
-    rawFile.onreadystatechange = function(){
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            userExists=isUser(this.responseText,username,password);
-        }
-        else if(rawFile.readyState==4 && rawFile.status=="404"){
-            $("#songs .container").html('<h1 style="text-align:center;">Error 404 </h1><h2>Oops!Sorry <br/>File Not found</h2>');
-        }
-    }
-    rawFile.open("GET","assests/js/login.json",false);
-    rawFile.send(null);
+}}
+)();
+
+$(document).ready(function(){
+	if($('#owl-demo-main').length){
+		$("#owl-demo-main").owlCarousel({
+		  items : 1, //10 items above 1000px browser width
+		  itemsDesktop: [1000,1], //5 items between 1000px and 901px
+		  itemsDesktopSmall : [900,1], // betweem 900px and 601px
+		  itemsTablet: [600,1], //2 items between 600 and 0
+		  itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+	});}
     
+    $('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').focus();
+});
     
-    if(userExists===true){
-        alert("Welcome "+username);
-     $(location)[0].href="test.html";
+
+    heightHoverBlocks();
+});
+
+
+function heightHoverBlocks(){
+    var hoverBox=$("figure figcaption");
+    for(var i=0;i<hoverBox.length;i++){
+    $(hoverBox[i]).css("height",hoverBox[i].previousElementSibling.height+"px");
+    $(hoverBox[i]).css("width",hoverBox[i].previousElementSibling.width+"px");    
     }
-    else{
-            alert("Wrong Username or password \n    SIGN IN");
-        }
-    }}
-
-
-function userSignIn(){
-    $(".sign_in").show();
-    $(".login_image ,.login_details").hide();
-    var newUserObj={
-        
-    };
-    var username=$("#firstName")[0].value;
-    var pwd=$("#pwd")[0].value;
-    if(username!=""&&pwd!=""){
-    alert("Welcome new user "+username);
-    $(location)[0].href="test.html";}
-}
-
-function isUser(userlist,username,password){
-    userlist=JSON.parse(userlist);
-    for(var i=0;i<userlist.users.length;i++){
-        if(userlist.users[i].userid===username&&
-          userlist.users[i].password===password)
-            return true;
-    }
-    return false;
-}
-
-function loginHomePage(){
-    $(location)[0].href="login.html";
+    
 }
